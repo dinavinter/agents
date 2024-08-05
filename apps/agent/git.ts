e51e57e6c9879668f709d2cabe421442c0576c84
dina <dina.vintr@gmail.com>
2024-08-06 01:36:10 +0300

    alt

---
+++ apps/agent/git.ts
@@ -0,0 +1,29 @@
+
+export type GithubLoaderParams = {repo: string, branch: string, accessToken?: string}
+export type GithubContentFile =  {url:string , path:string,  src:string; branch: string, repo: string} 
+export async function getRepoFiles({repo,branch, accessToken}: GithubLoaderParams):Promise<GithubContentFile[]> {
+       let url = `https://api.github.com/repos/${repo}/git/trees/${branch}?recursive=1`;
+
+        const headers = {
+            'Authorization': `Bearer ${accessToken}`,
+            'Accept': 'application/vnd.github.v3+json'
+        };
+
+        const response = await fetch(url, { headers }) ;
+        if (!response.ok) {
+            throw new Error(`Failed to fetch files: ${response.status} ${response.statusText}`);
+        }
+        const json = await  response.json()   as {tree: {
+                type: string,
+                path: string,
+                url: string
+            }[]};
+        const files = json.tree.filter((file) => file.type === 'blob' );
+        return files.map((file) => ({
+            ...file,
+            branch, repo,
+            src:`https://raw.githubusercontent.com/${repo}/${branch}/${file.path}`, 
+        })); 
+}
+
+
