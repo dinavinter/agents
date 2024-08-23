feece3e1e86369490a8658a65e8659b400a3e81f
dina <dina.vintr@gmail.com>
2024-08-23 03:25:28 +0300

    breadcrumbs

--- apps/agent/ui/htmx.ts
+++ apps/agent/ui/htmx.ts
@@ -48,15 +48,14 @@ export const layout = (agent:string, workflow:string) => `
 
        </head>
        <body> 
-        <header class="bg-slate-50 ticky top-0 z-10 backdrop-filter backdrop-blur bg-opacity-30 border-b border-gray-200 items-start justify-start py-2 ">
+        <header class="bg-slate-50 ticky top-0 z-10 backdrop-filter backdrop-blur  border-b border-gray-200 items-start justify-start py-2 ">
 
-           <div class="text-sm breadcrumbs"> 
-              <span class="text-gray-700">agents</span>
+           <div class="text-sm breadcrumbs *:hover:text-slate-500 *:text-gray-500 *:hover:shadow-sm"> 
+              <a href="/agents"  ">agents</a> 
               <span class="mx-2 text-gray-500">/</span> 
-              <a href="#" class="text-gray-500 hover:text-gray-700">${agent}</a> 
-              <a href="${workflow}" class="text-gray-500 hover:text-gray-700">${workflow}</a>
-              <span class="mx-2 text-gray-500">/</span>
-              
+              <a href="/agents/${agent}" >${agent}</a> 
+              <span class="mx-2 text-gray-500">/</span> 
+              <a href="#" class="text-slate-400 hover:text-slate-300">${workflow}</a> 
             </div>
         </header>
             <div hx-ext="sse" sse-connect="${workflow}" sse-swap="render" hx-swap="beforeend">
