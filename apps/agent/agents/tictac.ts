711888fe337feb8617ef406ef5b9acf3f3c5f687
dina <dina.vintr@gmail.com>
2024-08-26 15:33:38 +0300

    tictac

--- apps/agent/agents/tictac.ts
+++ apps/agent/agents/tictac.ts
@@ -1,27 +1,14 @@
-import {assign, setup, log, enqueueActions, emit} from 'xstate';
+import {assign, setup, log, emit} from 'xstate';
 import {z} from 'zod';
 import {fromAIEventStream, openaiGP4o} from "../ai";
 import {TextStreamPart, tool} from "ai";
-import {Html, render, RenderStream, renderTo, StreamOptions} from "../ui/render";
-import {c, html} from "atomico";
-import {Board, Player, Players} from '../ui/components/board';
+import { render, renderTo} from "../ui/render";
 
 
 type Player = 'x' | 'o';
 type Cell = Player | 'empty';
 type Board = Array<Cell>;
 
-
-/*thoughts
- <div>
-    <h2>AI X</h2>
-    <${stream.service('x').event("thought").text}/>
-</div>
-<div>
-    <h2>AI O</h2>
-    <${stream.service('o').event("thought").text}/>
-</div>
- */
 function isCellEmpty(cell: Cell): cell is "empty" {
     return cell === "empty";
 }
@@ -64,83 +51,7 @@ function changePlayer(player: Player): Player {
     return player === 'x' ? 'o' : 'x';
 }
 
-const renderBoardEnq = enqueueActions(({enqueue}) => {
-    enqueue(render(({stream, html}) => html`
-        <div ext="sse" sse-connect="${stream.href}/events">
-            <div class="grid grid-rows-3 grid-cols-3 gap-4" sse-swap="cell" hx-swap="beforeend"/>
-        </div>
-    `))
 
-    for (let i = 0; i < 9; i++) {
-        enqueue(renderTo("cell", ({stream, html}) => html`
-            <div class="w-16 h-16 flex justify-center items-center" ext="sse" id="${i}" sse-swap="${i}"
-                 hx-swap="textContent"></div>
-        `))
-    }
-})
-export const BoardHx = c(({src}) => {
-    const Cell = ({id}: { id: string }) => {
-        return html`
-            <div class="w-16 h-16 flex justify-center items-center" ext="sse" id="${id}" sse-swap="${id}">
-                <button hx-post=${id}" hx-target=${id} hx-swap-oob="${id}" hx-swap="outerHTML"
-                        class="btn btn-primary mt-2">
-                    Play
-                </button>
-            </div>`
-    }
-    const Row = ({id}: { id: string }) => {
-        return html`
-            <div class="grid grid-cols-3 gap-4">
-                <${Cell} id="${id}:0"></${Cell}>
-                <${Cell} id="${id}:1"></${Cell}>
-                <${Cell} id="${id}:2"></${Cell}>
-            </div>`
-    }
-
-    return html`
-        <host>
-            <div class="grid grid-cols-3 gap-4">
-                <${Row} id="0"></${Row}>
-                <${Row} id="1"></${Row}>
-                <${Row} id="2"></${Row}>
-            </div>
-        </host>`
-}, {
-    props: {
-        src: {type: String, value: ""}
-    }
-})
-
-const renderBoard = render(({stream, html}) => {
-    const Cell = (id: number) => {
-        return html`
-            <div class="w-16 h-16 flex justify-center items-center" ext="sse" id="${id}" sse-swap="${id}"
-                 hx-swap="textContent"/>`
-    }
-    const Row = (id: number) => {
-        id = id * 3;
-        return html`
-            <div class="grid grid-cols-3 gap-4">
-                ${Cell(id)}
-                ${Cell(id + 1)}
-                ${Cell(id + 2)}
-            </div>`
-    }
-
-    return html`
-        <main class="mx-auto  bg-slate-50">
-            <header class="sticky top-0 z-10 backdrop-filter backdrop-blur bg-opacity-30 border-b border-gray-200 flex h-6 md:h-14 items-center justify-center px-4 text-xs md:text-lg font-medium sm:px-6 lg:px-8">
-                Tic Tac Toe
-            </header>
-
-            <div class="grid grid-rows-3 gap-4" ext="sse" sse-connect="${stream.href}/events">
-                ${Row(0)}
-                ${Row(1)}
-                ${Row(2)}
-            </div>
-        </main>`
-
-})
 export const machine = setup({
     types: {
         context: {} as {
@@ -186,13 +97,13 @@ export const machine = setup({
         },
     },
 }).createMachine({
-    initial: 'setup',
+    initial: 'playing',
     context: {
         printedBoard: "",
         board: Array(9).fill('empty'),
         moves: 0,
         player: 'x' as Player,
-        history: []
+        history: [] 
     },
     meta: {
         name: 'TicTacToe',
@@ -200,40 +111,29 @@ export const machine = setup({
     },
 
     entry: render(({html, stream}) => html`
-        <main class="mx-auto  bg-slate-50 h-screen"  sse-connect="${stream.href}/events">
+        <main class="mx-auto  bg-slate-50 h-screen"  >
             <header class="sticky top-0 z-10 backdrop-filter backdrop-blur bg-opacity-30 border-b border-gray-200 flex h-6 md:h-14 items-center justify-center px-4 text-xs md:text-lg font-medium sm:px-6 lg:px-8">
                 Tic Tac Toe
             </header>
-            <div ext="sse" sse-swap="board"/>
-            <div ext="sse" sse-swap="report"  class="flex  flex-row-reverse *:p2 "/> 
+            <section class="p-5">
+                <div class="flex justify-center relative">
+                    <div class="grid grid-cols-3 grid-rows-3 *:w-20 *:h-20 *:md:w-32  *:md:h-32  *:text-3xl *:flex *:justify-center *:items-center ">
+                        <div sse-swap="c:0"/>
+                        <div sse-swap="c:1" class="border-x border-black"/>
+                        <div sse-swap="c:2"/>
+                        <div sse-swap="c:3" class="border-y border-black"/>
+                        <div sse-swap="c:4" class="border border-black"/>
+                        <div sse-swap="c:5" class="border-y border-black"/>
+                        <div sse-swap="c:6" />
+                        <div sse-swap="c:7" class="border-x border-black"/>
+                        <div sse-swap="c:8"/>
+                    </div>
+                </div>
+            </section>
+            <div hx-ext="sse" sse-swap="report"  class="flex  flex-row-reverse *:p2 "/> 
         </main>
     `),
-    states: {
-        setup: {
-            entry: renderTo('board', ({stream, html}) => html`
-                <section class="p-5">
-                    <div class="flex justify-center relative">
-                        <div sse-connect="${stream.href}/events/assign"
-                             class="grid grid-cols-3 grid-rows-3 *:w-20 *:h-20 *:md:w-32  *:md:h-32  *:text-3xl *:flex *:justify-center *:items-center ">
-                            <div sse-swap="0"/>
-                            <div sse-swap="1" class="border-x border-black"/>
-                            <div sse-swap="2"/>
-                            <div sse-swap="3" class="border-y border-black"/>
-                            <div sse-swap="4" class="border border-black"/>
-                            <div sse-swap="5" class="border-y border-black"/>
-                            <div sse-swap="6"/>
-                            <div sse-swap="7" class="border-x border-black"/>
-                            <div sse-swap="8"/>
-                        </div>
-                        <div sse-swap="win-line" hx-swap="outerHTML">
-                        </div>
-                    </div>
-                </section>
-            `),
-            after: {
-                500: 'playing'
-            }
-        },
+    states: { 
         playing: {
             always: [
                 {target: 'gameOver.winner', guard: 'checkWin'},
@@ -262,7 +162,7 @@ export const machine = setup({
                                 actions: [
                                     emit(({event: {args: {index}}}) => ({
                                         type: "assign",
-                                        event: index,
+                                        event: `c:${index}`,
                                         data: "x"
                                     })),
                                     {type: 'updateBoard', params: ({event: {args: {index}}}) => ({index, player: 'x'})},
@@ -271,13 +171,7 @@ export const machine = setup({
 
                             },
                             {target: 'x', reenter: true},
-                        ],
-                        // 'text-delta': {
-                        //     actions:emit( ({event:{textDelta}}) => ({
-                        //         type: '@x.thought',
-                        //          data: textDelta
-                        //     }))
-                        // }
+                        ] 
                     }
                 },
                 o: {
@@ -300,7 +194,7 @@ export const machine = setup({
                                 guard: {type: 'isValidMove', params: ({event: {args: {index}}}) => index},
                                 actions: [emit(({event: {args: {index}}}) => ({
                                     type: "assign",
-                                    event: index,
+                                    event:  `c:${index}`,
                                     data: "o"
                                 })), {
                                     type: 'updateBoard',
@@ -308,13 +202,7 @@ export const machine = setup({
                                 }]
                             },
                             {target: 'o', reenter: true},
-                        ],
-                        // 'text-delta': {
-                        //     actions:emit( ({event:{textDelta}}) => ({
-                        //         type: '@o.thought',
-                        //         data: textDelta
-                        //     }))
-                        // }
+                        ] 
                     }
                 }
             }
@@ -340,6 +228,12 @@ export const machine = setup({
                     tags: 'winner',
                 },
                 draw: {
+                    entry: renderTo('report', ({ html, stream}) => html`
+                        <div class="mt-4  block shadow-2xl mb-4 p-2 ">
+                            <h2 class="text-xl font-semibold float-left">It's a draw</h2>
+                          <div  >
+                       `),
+                        
                     tags: 'draw',
                 },
             }
