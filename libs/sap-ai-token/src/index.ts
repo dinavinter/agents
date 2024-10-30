dd41824c24ad57d62a96773bcbba610bda572d55
din <dina.vintr@gmail.com>
2024-10-30 18:01:34 +0200

    delete not agent projects

--- libs/sap-ai-token/src/index.ts
+++ libs/sap-ai-token/src/index.ts
@@ -1,16 +1,3 @@
-import {
-    assign,
-    createActor,
-    createMachine,
-    emit,
-    fromCallback,
-    fromPromise,   InputFrom,
-    log,
-    waitFor
-} from "xstate";
-import {Creds, tokenRequest} from "./request";
-
-
  
 export  * from './request'
 export  * from './service'
