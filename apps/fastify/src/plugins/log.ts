d7d00c5ca9afff0f0740367c6af01c26d9860cd9
din <dina.vintr@gmail.com>
2024-11-16 22:02:43 +0200

    logs

--- apps/fastify/src/plugins/log.ts
+++ apps/fastify/src/plugins/log.ts
@@ -1,17 +1,20 @@
 import type {FastifyPluginAsyncZod} from "fastify-type-provider-zod";
 import fp from "fastify-plugin";
-import rootLogger, {Framework, Level} from 'cf-nodejs-logging-support'
+import rootLogger, {CustomFieldsTypeConversion, Framework, Level} from 'cf-nodejs-logging-support'
  
 rootLogger.setFramework(Framework.Fastify);
  rootLogger.enableTracing()
+rootLogger.setCustomFieldsFormat("all");
+rootLogger.setCustomFieldsTypeConversion(CustomFieldsTypeConversion.Retain)
 
 const logPlugin: FastifyPluginAsyncZod = async function (fastify ) {
  
     // Add the logger middleware to write access logs
     fastify.addHook("onRequest", rootLogger.logNetwork)
 
-    fastify.addHook('onError', (request, _reply, error, done) => {
-        request.log.error('Unexpected error occured', error);
+    fastify.addHook ('onError', (request, _reply, error, done) => {
+        request.log.error('Request failed.', { "_error": error});
+        rootLogger.logError('Unexpected error occured',{ "_error": error});
         done();
     });
 
