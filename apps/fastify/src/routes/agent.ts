4b31550db5473be2ce217e37a899296143c0c70a
din <dina.vintr@gmail.com>
2024-12-17 01:12:59 +0200

    example module

--- apps/fastify/src/routes/agent.ts
+++ apps/fastify/src/routes/agent.ts
@@ -60,8 +60,63 @@ const routes: FastifyPluginAsync = async function (instance: FastifyInstance, op
                     }
                 })`
 
-    fastify.log.info('example', example)
+    const example2=`import {createMachine, assign, emit} from "xstate";
 
+
+export default createMachine({
+    id: 'machine',
+    initial: 'stage1', 
+    context:{
+        index: 0
+    },
+    states: {
+        stage1: {
+            entry: assign({
+                index: ({context: {index}}: { context: { index: number } }) => index + 1
+            }),
+            always: {
+                target: 'stage2',
+                guard: ({context: {index}}: { context: { index: number } }) => index > 10
+            }, 
+            after: {
+                1000: {
+                    target: 'stage1',
+                    reenter: true,
+                    actions: emit(({context: {index}}: { context: { index: number } }) => ({
+                        type: 'EMIT',
+                        data: index,
+                        event: 'stage-1'
+                    }))
+                },
+                
+
+            },
+        },
+        stage2:{
+            entry: assign({
+                index: ({context: {index}}: { context: { index: number } }) => index + 1
+            }),
+            after: {
+                10000: {
+                    target: 'stage2',
+                    reenter: true,
+                    actions: emit(({context: {index}}: {context:{index:number}}) => ({
+                        type: 'EMIT',
+                        data: index,
+                        event: 'stage-2'
+                    }))
+                }
+            },
+        }
+    }
+})
+
+ 
+
+
+ 
+`
+ 
     fastify.route({
         url: '/agents',
         method: 'GET',
@@ -128,7 +183,7 @@ const routes: FastifyPluginAsync = async function (instance: FastifyInstance, op
                     code: {
                         type: 'string',
                         description: 'The agent javascript code',
-                        examples: [example]
+                        examples: [example, example2]
                     }
                 }
             },
@@ -179,7 +234,7 @@ const routes: FastifyPluginAsync = async function (instance: FastifyInstance, op
                     code: {
                         type: 'string',
                         description: 'The agent javascript code',
-                        examples: [example]
+                        examples: [example2,example]
                     }
                 }
             },
