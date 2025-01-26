ea06995dbb2ac0d3feb7995ccbe25239b24c9a88
din <dina.vintr@gmail.com>
2025-01-26 19:12:18 +0200

    ideas

---
+++ apps/function/examples/ideas.md
@@ -0,0 +1,13 @@
+polygraph
+
+customized podcast
+
+customized press
+
+ai newsletter
+
+ai podcast
+
+resealing 
+
+
--- libs/streaming/src/ui/components/chatBubble.ts
+++ libs/streaming/src/ui/components/chatBubble.ts
@@ -1,7 +1,7 @@
 import {c, css, html} from "atomico";
 
 export const ChatBubble = c(({ content, name, img, swap }) => {
-    return html`<div class="flex items-start gap-4 p-4 m-4 w-full bg-gray-50 dark:bg-gray-800 rounded-lg shadow">
+    return html`<div shadowDom class="flex items-start gap-4 p-4 m-4 w-full bg-gray-50 dark:bg-gray-800 rounded-lg shadow">
         <img class="w-12 h-12 rounded-full" src=${img} alt=${name}  />
         <div class="flex flex-col gap-2 w-full">
             <div class="flex items-center space-x-2 rtl:space-x-reverse">
@@ -9,7 +9,12 @@ export const ChatBubble = c(({ content, name, img, swap }) => {
                 <span class="text-sm font-normal text-gray-500 dark:text-gray-400" sse-swap="@${swap}.status" hx-swap="innerHTML"></span>
             </div>
             <div class="p-4 border border-gray-200 bg-white dark:bg-gray-700 rounded-lg">
-                <pre class="text-sm text-gray-900 dark:text-gray-300 whitespace-pre-wrap" sse-swap="${swap}" hx-swap="innerHTML">${content}</pre>
+                <pre class="text-sm text-gray-900 dark:text-gray-300 whitespace-pre-wrap" sse-swap="${swap}" hx-swap="innerHTML">
+                    ${content}
+                </pre>
+                <slot>
+                    
+                </slot>
             </div>
         </div>
     </div>`;
